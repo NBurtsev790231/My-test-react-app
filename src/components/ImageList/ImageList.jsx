69c3a67087fc47df3ee8d8878232af7769f314b7
Nikita790231 <nburtsev7922@gmail.com>
2023-10-26 23:19:36 +0300

    Реализован список изображений, нуждается в доработке

--- src/components/ImageList/ImageList.jsx
+++ src/components/ImageList/ImageList.jsx
@@ -1,9 +1,104 @@
-import { ImageListWrapper } from './styles';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/alt-text */
+import { useEffect, useRef, useState } from 'react';
+
+import { Form } from '../Form';
+
+import { DeleteImage, ImageDate, ImageItem, ImageListWrapper, ImageName, ImagePhoto, ImageTextArea } from './styles';
+//import { Delete } from './img/delete.svg';
+
 
 export const ImageList = () => {
+  const [ImageList, setImageList] = useState([]);
+
+  const [codeStatus, setCodeStatus] = useState('');
+
+  const url = 'https://api.unsplash.com/photos/random/?query=parks&count=5&client_id=ue66y_t3CMluJDMhaNONYCxoVmRZCFUTbyH9Sjlj-jA';
+
+  const effectCalled = useRef(false);
+
+
+//Реализация списка изображений
+  const MyNewImage = (description, link, comment) => {
+    let date = (new Date()).toLocaleDateString('ru', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    })
+
+    setImageList([...ImageList, { 
+      id: Math.round(Math.random()), 
+      date: date, 
+      name: description, 
+      url: link, 
+      comment: comment }]);
+  };
+
+
+  const fetchPhoto = async () => {
+    try {
+      const res = await fetch(url);
+      const json = await res.json();
+
+      if (res.status === 200) {
+        for (let i = 0; i < Object.keys(json).length; i++) {
+          let date = (new Date (json[i].created_at)).toLocaleDateString('ru', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+          })
+          let description = json[i].description !==null ? json[i].description: json[i].alt_description
+
+          setImageList(item => [...item, {
+            id: json[i].id + Math.round(Math.random()), 
+            url: json[i].urls.regular, 
+            date: date, 
+            name: description}])
+        }
+      }
+
+      //Проверка статуса на ошибку
+      else 
+        {setCodeStatus(`Статус: ${res.status} Ошибка: ${json.errors}`)}
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+
+  useEffect(() => {
+    if (effectCalled.current === false) {
+       fetchPhoto()
+    }
+
+    return () => effectCalled.current = true;
+  }, []);
+
+
+//Вывод списка изображений
   return (
+    <>
+    <Form NewPhoto={(name, link, comment) => MyNewImage(name, link, comment)}/>
+    {codeStatus}
     <ImageListWrapper>
-      <li>Image1</li>
+      {ImageList.map((Image) =>
+      <ImageItem>
+        <li key={Image.id}> 
+        <DeleteImage onClick={() => setImageList(ImageList.filter(value => value !== Image))}>
+          
+        </DeleteImage>
+        <ImageName>{Image.name}</ImageName>
+        <ImageDate>Дата добавления: <a>{Image.date}</a></ImageDate>
+        <ImagePhoto className={ImageListWrapper.Image} src={Image.url} alt='#'/>
+        <ImageTextArea name='description' value={Image.comment !== undefined ? 
+          ('Описание: ' + Image.comment): ''} disabled={true}></ImageTextArea>
+        </li>
+      </ImageItem>
+      )};
     </ImageListWrapper>
+    </>
   );
 };
+
+
+//<img src={Delete}/>
--- src/components/ImageList/styles.js
+++ src/components/ImageList/styles.js
@@ -1,5 +1,72 @@
 import styled from 'styled-components';
 
 export const ImageListWrapper = styled.div`
-  background-color: red;
+display: flex;
+flex-wrap: wrap;
+max-width: 1500px;
+justify-content: center;
+margin:10px auto;
+border: 2px solid #7442c8;
+`;
+
+export const ImageTextArea = styled.textarea`
+  width:340px;
+  height:60px;
+  background-color: white;
+  border:none;
+  resize: none;
+  color:black;
+  font-size:16px;
+  font-family: "Times New Roman", Times, serif;
+  border: 1px solid #000000;
+`;
+
+export const ImageName = styled.h1`
+  font-size:24px;
+  margin:0px 0px 10px;
+  width:330px;
+  height:55px;
+  overflow: hidden; 
+  word-wrap: break-word;
+`;
+
+export const ImageDate = styled.h2`
+  font-size:18px;
+  color:gray;
+  margin:0px 0px 10px;
+a{
+  color:black;
+}
+`;
+
+
+export const ImagePhoto = styled.img`
+  width: 450px;
+  height: 325px;
+  border-radius:10px;
+  border: 1px solid #000000;
+`;
+
+export const ImageItem = styled.div`
+  position: relative;
+  list-style: none;
+  background-color: white;
+  box-shadow: 0px 2px 2px 2px gray;
+  border-radius: 5px;
+  margin:0px 10px 20px;
+  padding:10px 10px;
+  width: 500px;
+  height: 500px;
+`;
+
+export const DeleteImage = styled.button`
+  position: absolute;
+  right: 5px;
+  background-color: rgba(0,0,0,0);
+  border:none;
+  width:40px;
+
+ &:hover ${this} {
+  color: blue;
+}
 `;
